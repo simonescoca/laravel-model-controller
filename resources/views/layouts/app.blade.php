455424814d6385ecc447260e1c7d0002bd8db5fe
simonescoca <simone.scoca@icloud.com>
2023-07-20 16:20:08 +0200

    created cards in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @include("partials.ex1")
-    @include("partials.ex2")
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
 
+        <main>
+            <div class="container d-flex flex-wrap justify-content-between py-5" style="gap: 1rem;">
+                @foreach ($movies_data as $movie_data)
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $movie_data -> title }}
+                            </h5>
+                            <p class="card-text">
+                                Nationality: {{ $movie_data -> nationality }}
+                            </p>
+                            <p class="card-text">
+                                Date: {{ $movie_data -> date }}
+                            </p>
+                            <p class="card-text">
+                                Vote: {{ $movie_data -> vote }}
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </main>
 
-
-    @vite("resources/js/app.js")
-</body>
+        @vite("resources/js/app.js")
+    </body>
 </html>
